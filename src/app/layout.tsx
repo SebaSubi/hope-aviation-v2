6bb1a861d340e7b94d594ca623d9c04a8935da1a
elianlehoux <elian.lehoux@uap.edu.ar>
2025-04-29 18:42:46 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <main className="min-h-screen bg-gray-50">
-            {children}
+          {children}
           </main>
         </Providers>
       </body>
